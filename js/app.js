3076a09f0158c3a33728552fd907b4f34ddd3103
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-05-22 20:49:41 -0400

    Add game logic and score increment functionality

--- js/app.js
+++ js/app.js
@@ -1,22 +1,50 @@
+var equationElement = document.getElementById('equation');
+var operands = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var deviation = [-1, 0, 0, 1];
+var compareSums;
 
 function generateEquation() {
-   var equationElement = document.getElementById('equation');
-   //var equationConent = equationElement.innerHTML;
-
-   var operands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-
-   var operand1 = operands[Math.floor(Math.random() * 10)];
-
-   var operand2 = operands[Math.floor(Math.random() * 10)];
+   var operand1 = operands[Math.floor(Math.random() * operands.length)];
+   var operand2 = operands[Math.floor(Math.random() * operands.length)];
    var correctSum = operand1 + operand2;
 
-   var deviation = [-1, 0, 1];
-   var showingSum = correctSum + deviation[Math.floor(Math.random() * deviation.length)];
+   var shownSum = correctSum + deviation[Math.floor(Math.random() * deviation.length)];
 
-   var resultString = operand1.toString() + " + " + operand2.toString() + " = " + showingSum.toString();
+   var resultString = operand1.toString() + " + " + operand2.toString() + " = " + shownSum.toString();
 
-   console.log(resultString);
-   equationConent = resultString;
+   //console.log(resultString);
+   equationElement.innerHTML = resultString;
+   compareSums = (correctSum == shownSum);
+   //console.log(compareSums);
+}
+
+var chooseCorrectEle = document.getElementById("choose-correct");
+var chooseWrongEle = document.getElementById("choose-wrong");
+
+
+chooseCorrectEle.addEventListener("click", function() {
+    if (compareSums == true) {
+        scoreIncrement();
+        generateEquation();
+    } else {
+        // game over screen
+    }
+});
+
+chooseWrongEle.addEventListener("click", function() {
+    if (compareSums == false) {
+        scoreIncrement();
+        generateEquation();
+    } else {
+        // game over screen
+    }
+});
+
+var scoreEle = document.getElementById("score");
+function scoreIncrement() {
+    var score = parseInt(scoreEle.innerHTML, 10);
+    score++;
+    scoreEle.innerHTML = score.toString();
 }
 
 generateEquation();
