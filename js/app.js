883cad477b4724f40223fdc5254eec71199a17a9
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-05-23 22:36:42 -0400

    Add html and css to replicate 'freaking math' appearance

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function generateEquation() {
 
    var shownSum = correctSum + deviation[Math.floor(Math.random() * deviation.length)];
 
-   var resultString = operand1.toString() + " + " + operand2.toString() + " = " + shownSum.toString();
+   var resultString = operand1.toString() + " + " + operand2.toString() + "<br> = " + shownSum.toString();
 
    //console.log(resultString);
    equationElement.innerHTML = resultString;
@@ -18,21 +18,23 @@ function generateEquation() {
    //console.log(compareSums);
 }
 
-var chooseCorrectEle = document.getElementById("choose-correct");
-var chooseWrongEle = document.getElementById("choose-wrong");
+var chooseCorrectEle = document.getElementById("choice-correct");
+var chooseWrongEle = document.getElementById("choice-wrong");
 var lastStart;
+var timeOutFlag = false;
 
 chooseCorrectEle.addEventListener("click", function() {
-    lastStart = Date.now();
     if (compareSums == true) {
         correctSound.play();
         scoreIncrement();
         generateEquation();
+        $('#progress-bar').find('div').width('100%');
+        progress(1, 1, $('#progress-bar'));
     } else {
         // game over screen
         wrongSound.play();
         updateBestScore(score);
-        //scoreEle.innerHTML = "0";
+        modal.style.display = "block";
     }
 });
 
@@ -42,13 +44,31 @@ chooseWrongEle.addEventListener("click", function() {
         correctSound.play();
         scoreIncrement();
         generateEquation();
+        $('#progress-bar').find('div').animate({width: '100%'}, 0);
+        progress(1, 1, $('#progress-bar'));
     } else {
         // game over screen
         wrongSound.play();
         updateBestScore(score);
-        //scoreEle.innerHTML = "0";
+        modal.style.display = "block";
     }
 });
+
+function progress(timeLeft, timeTotal, $element) {
+    var progressBarWidth = $element.width() * (timeLeft / timeTotal);
+    $element.find('div').animate({width: 0}, 1000);
+
+    /*
+    if (timeLeft > 0) {
+        setTimeout(function() {
+            progress(timeLeft - 1, timeTotal, $element);
+        }, 1000);
+    } else {
+        //modal.style.display = "block";
+    }
+    /**/
+    
+}
 
 var scoreEle = document.getElementById("score");
 var score = parseInt(scoreEle.innerHTML, 10);
@@ -71,10 +91,15 @@ function updateBestScore(currentScore) {
 //    start = Date.now(); 
 //}
 var soundOnOffEle = document.getElementById("sound");
+var soundIconEle = soundOnOffEle.children[0];
+
 soundOnOffEle.addEventListener("click", function() {
     correctSound.soundOnOff();
     wrongSound.soundOnOff();
 });
+
+var correctSound = new Sound('sound/correct.mp3');
+var wrongSound = new Sound('sound/wrong.mp3');
 
 function Sound(source) {
     this.sound = document.createElement("audio");
@@ -89,12 +114,37 @@ Sound.prototype.play = function() {
 Sound.prototype.soundOnOff = function() {
     if (this.sound.muted == false) {
         this.sound.muted = true;
+        soundIconEle.setAttribute("class", "fas fa-volume-off");
     } else {
         this.sound.muted = false;
+        soundIconEle.setAttribute("class", "fas fa-volume-down");
     }
 };
 
-var correctSound = new Sound('sound/correct.mp3');
-var wrongSound = new Sound('sound/wrong.mp3');
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 generateEquation();
