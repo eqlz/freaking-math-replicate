7825992436b8af675558e9c6a3eee448060f582a
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-05-24 15:50:41 -0400

    Add countdown bar, game start modal, game end modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,149 @@
-var equationElement = document.getElementById('equation');
+/* 
+ * Set up event listener for start button in initial game page
+ */
+var gameStartModal = document.getElementById("game-start-modal");
+document.getElementById("game-start-button").addEventListener("click", function() {
+    gameStartModal.style.display = "none";
+});
+
+/*
+ * Load the page
+ */
+var score,
+    bestScore;
+
+var checkCard = document.getElementById("choice-correct");
+var crossCard = document.getElementById("choice-wrong");
+
+window.onload = function() {
+    gameStartModal.style.display = "block";
+
+    score = 0;
+
+    // If localStorage exists, update its value. If not, set up localStorage.
+    if (localStorage.getItem("bestScore") != null) {
+        bestScore = parseInt(localStorage.getItem("bestScore"), 10);
+    } else {
+        bestScore = 0;
+        localStorage.setItem("bestScore", bestScore);
+    }
+
+    setBackgroundColor();
+    generateEquation();
+    
+    checkCard.addEventListener("click", function() {
+        setTimeout(function() {
+            checkCard.setAttribute("class", "card clicked-correct");
+        }, 10);
+
+        if (compareSums == true) {
+            correctSound.play();
+
+            setTimeout(function() {
+                checkCard.setAttribute("class", "card judge-right");
+            }, 50);
+
+            nextGame();    
+        } else {
+            wrongSound.play();
+
+            setTimeout(function() {
+                checkCard.setAttribute("class", "card judge-wrong");
+            }, 50);
+
+            showGaveOver(false);
+        }
+    });
+
+    crossCard.addEventListener("click", function() {
+        setTimeout(function() {
+            crossCard.setAttribute("class", "card clicked-wrong");
+        }, 10);
+
+        if (compareSums == false) {
+            correctSound.play();
+
+            setTimeout(function() {
+                crossCard.setAttribute("class", "card judge-right");
+            }, 50);
+
+            nextGame();
+        } else {
+            wrongSound.play();
+
+            setTimeout(function() {
+                crossCard.setAttribute("class", "card judge-wrong");
+            }, 50);
+
+            showGaveOver(false);
+        }
+    });
+}
+
+/*
+ * Start a new game when a user judges equation correctly
+ */
+function nextGame() {
+    clearTimeout(timeOut);
+    clearInterval(progressTimer);
+    incrementScore();
+    generateEquation();
+    runCountDown();
+}
+
+/*
+ * Show 'Game Over' or 'Time Out' modal, depends on how a user loses the game
+ */
+var gameEndModal = document.getElementById('game-end-modal');
+
+function showGaveOver(outOfTime) {
+    clearTimeout(timeOut);
+    clearInterval(progressTimer);
+
+    if (outOfTime) {
+        document.getElementById("game-end-message").innerHTML = "Time Out";
+    } else {
+        document.getElementById("game-end-message").innerHTML = "Game Over";
+    }
+
+    // Update scores that will be shown in game end modal
+    document.getElementById("new-score").innerHTML = score;
+    updateBestScore(score);
+    document.getElementById("best-score").innerHTML = localStorage.getItem("bestScore");
+
+    gameEndModal.style.display = "block";
+}
+
+/*
+ * Set up event when restart button is clicked
+ */
+document.getElementById("restart-button").addEventListener("click", function() {
+    gameEndModal.style.display = "none";
+
+    score = 0
+    document.getElementById("score").innerHTML = score;
+    generateEquation();
+    runCountDown();
+});
+
+/*
+ * Set up event when home button is clicked
+ */
+document.getElementById("home-button").addEventListener("click", function() {
+    gameEndModal.style.display = "none";
+
+    score = 0
+    document.getElementById("score").innerHTML = score;
+    generateEquation();
+
+    gameStartModal.style.display = "block";
+});
+
+/*
+ * Generate equation for a user to judge
+ */
 var operands = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var deviation = [-1, 0, 0, 1];
+var deviation = [-1, 0, 0, 0];
 var compareSums;
 
 function generateEquation() {
@@ -12,84 +155,79 @@ function generateEquation() {
 
    var resultString = operand1.toString() + " + " + operand2.toString() + "<br> = " + shownSum.toString();
 
-   //console.log(resultString);
-   equationElement.innerHTML = resultString;
+   document.getElementById('equation').innerHTML = resultString;
    compareSums = (correctSum == shownSum);
-   //console.log(compareSums);
 }
 
-var chooseCorrectEle = document.getElementById("choice-correct");
-var chooseWrongEle = document.getElementById("choice-wrong");
-var lastStart;
-var timeOutFlag = false;
+/*
+ * Run countdown progress bar after a user judge equation correctly
+ */
+var progressBar,
+    progressTimer,
+    timeOut
 
-chooseCorrectEle.addEventListener("click", function() {
-    if (compareSums == true) {
-        correctSound.play();
-        scoreIncrement();
-        generateEquation();
-        $('#progress-bar').find('div').width('100%');
-        progress(1, 1, $('#progress-bar'));
-    } else {
-        // game over screen
+function runCountDown() {
+    progressBar = document.getElementById("progress-bar-change");
+    progressBar.style.width = "100%";
+
+    progressTimer = setInterval(function() {
+        switch(progressBar.style.width) {
+            case "100%":
+                progressBar.style.width = "66.6%";
+                break;
+            case "66.6%":
+                progressBar.style.width = "33.3%";
+                break;
+            case "33.3%":
+                progressBar.style.width = "0%";
+                break;
+            default:
+                progressBar.style.width = "100%";
+                break;
+        }
+    }, 500);
+
+    timeOut = setTimeout(function() {
         wrongSound.play();
-        updateBestScore(score);
-        modal.style.display = "block";
-    }
-});
 
-chooseWrongEle.addEventListener("click", function() {
-    lastStart = Date.now();
-    if (compareSums == false) {
-        correctSound.play();
-        scoreIncrement();
-        generateEquation();
-        $('#progress-bar').find('div').animate({width: '100%'}, 0);
-        progress(1, 1, $('#progress-bar'));
-    } else {
-        // game over screen
-        wrongSound.play();
-        updateBestScore(score);
-        modal.style.display = "block";
-    }
-});
+        checkCard.setAttribute("class", "card judge-wrong");
+        crossCard.setAttribute("class", "card judge-wrong");
 
-function progress(timeLeft, timeTotal, $element) {
-    var progressBarWidth = $element.width() * (timeLeft / timeTotal);
-    $element.find('div').animate({width: 0}, 1000);
-
-    /*
-    if (timeLeft > 0) {
-        setTimeout(function() {
-            progress(timeLeft - 1, timeTotal, $element);
-        }, 1000);
-    } else {
-        //modal.style.display = "block";
-    }
-    /**/
-    
+        showGaveOver(true);
+    }, 1500);
 }
 
-var scoreEle = document.getElementById("score");
-var score = parseInt(scoreEle.innerHTML, 10);
-function scoreIncrement() {
+/*
+ * Choose background color randomly from array backgroundColor
+ */
+var backgroundColor = ["#F39C12", "#8E44AD", "#27AE60", "#16A085", "#7F8C8D"];
+
+function setBackgroundColor() {
+    var color = backgroundColor[Math.floor(Math.random() * backgroundColor.length)];
+    document.body.style.backgroundColor = color;
+}
+
+/*
+ * Increment score
+ */
+function incrementScore() {
     score++;
-    scoreEle.innerHTML = score.toString();
+    document.getElementById("score").innerHTML = score;
 }
 
-localStorage.setItem("bestScore", 0);
-
+/*
+ * Update best score in localStorage when a game is over
+ */
 function updateBestScore(currentScore) {
-    var bestScore = localStorage.getItem("bestScore");
     if (currentScore > bestScore) {
-        localStorage.setItem("bestScore", currentScore);
+        bestScore = currentScore;
+        localStorage.setItem("bestScore", bestScore);       
     }
 }
 
-
-//function checkRemainingTime() {
-//    start = Date.now(); 
-//}
+/*
+ * Enable sound effect, and turning sound on and off
+ */
 var soundOnOffEle = document.getElementById("sound");
 var soundIconEle = soundOnOffEle.children[0];
 
@@ -98,6 +236,7 @@ soundOnOffEle.addEventListener("click", function() {
     wrongSound.soundOnOff();
 });
 
+// Create two sound effects for judging equation correctly or wrongly
 var correctSound = new Sound('sound/correct.mp3');
 var wrongSound = new Sound('sound/wrong.mp3');
 
@@ -120,31 +259,3 @@ Sound.prototype.soundOnOff = function() {
         soundIconEle.setAttribute("class", "fas fa-volume-down");
     }
 };
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-generateEquation();
